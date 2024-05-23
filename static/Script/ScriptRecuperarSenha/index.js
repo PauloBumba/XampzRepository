d4c3af66e9a57d16ca1117faa521146c37c2fb37
Paulo Mário Valente Bumba <143729787+PauloBumba@users.noreply.github.com>
2024-05-23 19:17:56 -0300

    concluído

--- static/Script/ScriptRecuperarSenha/index.js
+++ static/Script/ScriptRecuperarSenha/index.js
@@ -1,7 +1,12 @@
 
 $(document).ready(function () {
    
+    $(window).resize(function (){
+        if($(window).innerWidth()>=769){
+            $("menu").show();
 
+        }
+    })
     $(".fa-bars").click(function () {
          
         $(this).toggleClass("fa-bars fa-times");
