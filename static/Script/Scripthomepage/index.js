613fdaa8bcc95a09ee898828da84e7572f814258
Paulo Mário Valente Bumba <143729787+PauloBumba@users.noreply.github.com>
2024-06-11 19:12:09 -0300

    header para diferentes telas

---
+++ static/Script/Scripthomepage/index.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function () {
+   
+    $(window).resize(function (){
+        if($(window).innerWidth()>=769){
+            $("#menu").show();
+
+        }
+        else{
+            $("#menu").hide();
+
+        }
+    })
+    $(".fa-bars").click(function () {
+         
+        $(this).toggleClass("fa-bars fa-times");
+        $("#menu").toggle();
+    });
+});
