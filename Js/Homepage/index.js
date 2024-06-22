e970ad3cc57c3d1ae0eda791687e1cf84afead7d
Paulo Mário Valente Bumba <143729787+PauloBumba@users.noreply.github.com>
2024-06-21 21:40:34 -0300

    concluida a versão mobile com sucesso

--- Js/Homepage/index.js
+++ Js/Homepage/index.js
@@ -17,3 +17,31 @@ $(document).ready(function () {
         $("#menu").toggle();
     });
 });
+
+
+const animacao = new ScrollReveal();
+animacao.reveal('.dish', {
+    origin: 'left',
+    distance: '30%',
+    duration:2000,
+    delay:500,
+    
+    reset :true
+});
+animacao.reveal('#home', {
+    origin: 'left',
+    distance: '30%',
+    duration:2000,
+    
+    
+    reset :true
+});
+animacao.reveal('.feedback', {
+    origin: 'rigth',
+    distance: '30%',
+    duration:2000,
+    delay:700,
+    
+    reset :true
+});
+
