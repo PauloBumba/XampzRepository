af6fc30129902015c7c584df1a2e9cd02c9c36d3
Paulo Mário Valente Bumba <143729787+PauloBumba@users.noreply.github.com>
2024-07-10 16:22:18 -0300

    update

--- Js/Cadastro/ShowPassword.js
+++ Js/Cadastro/ShowPassword.js
@@ -3,13 +3,13 @@ $(document).ready(function () {
         var $senhaInput = $('#senha');
         var type = $senhaInput.attr('type') === 'password' ? 'text' : 'password';
         $senhaInput.attr('type', type);
-        $(this).text(type === 'password' ? 'Mostrar' : 'Ocultar');
+        $(this).val(type === 'password' ? 'Mostrar' : 'Ocultar');
     });
 
     $('#toggleConfirmaSenha').on('click', function () {
         var $confirmaSenhaInput = $('#confirma_senha');
         var type = $confirmaSenhaInput.attr('type') === 'password' ? 'text' : 'password';
         $confirmaSenhaInput.attr('type', type);
-        $(this).text(type === 'password' ? 'Mostrar' : 'Ocultar');
+        $(this).val(type === 'password' ? 'Mostrar' : 'Ocultar');
     });
 })
